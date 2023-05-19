9627dd60a72e411ca27ad1dd5a5f1183c2cc5f9e
Dmitrii Sobin <neo7976@yandex.ru>
2023-05-19 11:05:38 +0300

    WagonPassport - добавили получение по номеру вагона

--- src/main/java/ru/sobinda/RZDInterview/repository/WagonPassportRepository.java
+++ src/main/java/ru/sobinda/RZDInterview/repository/WagonPassportRepository.java
@@ -3,5 +3,12 @@ package ru.sobinda.RZDInterview.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sobinda.RZDInterview.entity.WagonPassportEntity;
 
+import java.util.Optional;
+
 public interface WagonPassportRepository extends JpaRepository<WagonPassportEntity, Integer> {
+
+
+    boolean existsByNumber(int number);
+
+    Optional<WagonPassportEntity> findByNumber(int number);
 }
